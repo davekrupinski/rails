c8ea6d67e93aceb9e747bc56339d5877a86dc2a8
Guillermo Iguaran <guilleiguaran@gmail.com>
2017-05-13 20:52:34 -0500

    Fix Postgres dump test with ignored tables

--- activerecord/test/cases/tasks/postgresql_rake_test.rb
+++ activerecord/test/cases/tasks/postgresql_rake_test.rb
@@ -260,7 +260,7 @@ if current_adapter?(:PostgreSQLAdapter)
       end
 
       def test_structure_dump_with_ignore_tables
-        ActiveRecord::SchemaDump.expects(:ignore_tables).returns([:foo, "bar"])
+        ActiveRecord::SchemaDumper.expects(:ignore_tables).returns(["foo", "bar"])
 
         Kernel.expects(:system).with("pg_dump", "-s", "-x", "-O", "-f", @filename, "-T", "foo", "-T", "bar", "my-app-db").returns(true)
 
